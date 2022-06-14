0b3606c0729621bda7524807a4d36f3102fd8d93
Derrick <derrickryan10@gmail.com>
2022-06-14 09:31:02 -0400

    Add button and message

--- index.js
+++ index.js
@@ -36,3 +36,55 @@ const gameBoard = (() => {
 
     return {setBox, getBox, reset};
 })();
+
+//gameController module
+const gameController = (() => {
+    const playerX = Player("X");
+    const playerO = Player("O");
+
+    const checkWin = (index) => {
+        const winConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        let gameWon = false;
+
+        for(let i = 0; i < winConditions.length; i++){
+            const winCondition = winConditions[i];
+            const cell1 = gameBoard.board[winCondition[0]];
+            const cell2 = gameBoard.board[winCondition[1]];
+            const cell3 = gameBoard.board[winCondition[2]];
+
+            if(cell1 === "" || cell2 === "" || cell3 === "") continue;
+            if(cell1 === cell2 && cell2 === cell3){
+                gameWon = true;
+                break;
+            }
+        }
+
+        if(gameWon){
+            //winner
+        }
+        else if(!gameBoard.includes("")){
+            //draw
+        }
+        else{
+            //change player
+        }
+    };
+    
+})();
+
+//displayController module
+const displayController = (() => {
+    
+
+
+})();
+
