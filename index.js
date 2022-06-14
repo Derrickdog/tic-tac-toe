6c1a7177df8a156af41bb846e513c52e8d6adcbf
Derrick <derrickryan10@gmail.com>
2022-06-14 10:09:16 -0400

    Finish buggy version

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// const boxes = document.querySelectorAll(".box");
-// boxes.forEach(box => box.addEventListener("click", changeBox));
-
-// function changeBox(e){
-//     e.target.textContent = "X";
-// }
-
 //player factory function
 const Player = (turn) => {
     this.turn = turn;
@@ -21,20 +14,20 @@ const gameBoard = (() => {
     const setBox = (index, sign) => {
         if(index > board.length) return;
         board[index] = sign;
-    }
+    };
 
     const getBox = (index) => {
         if(index > board.length) return;
         return board[index];
-    }
+    };
 
     const reset = () => {
-        for(let box of board){
-            box = "";
+        for(let i = 0; i < board.length; i++){
+            board[i] = "";
         }
-    }
+    };
 
-    return {setBox, getBox, reset};
+    return {setBox, getBox, reset, board};
 })();
 
 //displayController module
@@ -44,19 +37,20 @@ const displayController = (() => {
     const resetBtn = document.getElementById("resetBtn");
 
     boxes.forEach((box) => box.addEventListener("click", (e) => {
-            gameController.playRound(parseInt(e.target.dataset.index));
+            gameController.playRound(parseInt(e.target.id.charAt(3))-1);
             updateBoard();
         })
     );
     
     resetBtn.addEventListener("click", () => {
         gameBoard.reset;
+        updateBoard();
         setStatusMsg("X Turn");
     });
 
     const updateBoard = () => {
         for(let i = 0; i < boxes.length; i++){
-            boxes[i].textContent = gameBoard.getBox(i);
+            boxes[i].textContent = gameBoard.board[i];
         }
     }
 
@@ -71,6 +65,16 @@ const displayController = (() => {
 const gameController = (() => {
     const playerX = Player("X");
     const playerO = Player("O");
+    let round = 1;
+
+    const playRound = (index) => {
+        gameBoard.setBox(index, getCurrentTurn());
+        checkWin();
+    };
+
+    const getCurrentTurn = () => {
+        return round % 2 === 1 ? playerX.getTurn() : playerO.getTurn();
+    }
 
     const checkWin = (index) => {
         const winConditions = [
@@ -99,14 +103,18 @@ const gameController = (() => {
         }
 
         if(gameWon){
-            //winner
+            displayController.setStatusMsg("X Wins!");
+            round = 1;
         }
-        else if(!gameBoard.includes("")){
-            //draw
+        else if(!gameBoard.board.includes("")){
+            displayController.setStatusMsg("Draw!");
+            round = 1;
         }
         else{
-            //change player
+            round++;
+            displayController.setStatusMsg(getCurrentTurn() + " Turn");
         }
     };
 
+    return { playRound, getCurrentTurn };
 })();
