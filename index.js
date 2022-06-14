d5b8308c2f2df37a77e1eb96079727fcbae77928
Derrick <derrickryan10@gmail.com>
2022-06-14 08:57:37 -0400

    Add person and gameBoard

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
-const boxes = document.querySelectorAll(".box");
-boxes.forEach(box => box.addEventListener("click", changeBox));
+// const boxes = document.querySelectorAll(".box");
+// boxes.forEach(box => box.addEventListener("click", changeBox));
 
-function changeBox(e){
-    e.target.textContent = "X";
+// function changeBox(e){
+//     e.target.textContent = "X";
+// }
+
+//player factory function
+const Player = (turn) => {
+    this.turn = turn;
+    const getTurn = () => {
+        return turn;
+    }
+    return {getTurn};
 }
+
+//gameBoard module
+const gameBoard = (() => {
+    const board = ["", "", "", "", "", "", "", "", ""];
+
+    const setBox = (index, sign) => {
+        if(index > board.length) return;
+        board[index] = sign;
+    }
+
+    const getBox = (index) => {
+        if(index > board.length) return;
+        return board[index];
+    }
+
+    const reset = () => {
+        for(let box of board){
+            box = "";
+        }
+    }
+
+    return {setBox, getBox, reset};
+})();
